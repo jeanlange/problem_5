3eb4693d8c0371a28c4bbc7e85008651a14e8d04
Jean Lange <langeje@us.ibm.com>
2018-04-20 23:37:45 -0400

    rework for 1

--- lib/desired_number.rb
+++ lib/desired_number.rb
@@ -1,8 +1,21 @@
 class DesiredNumber
-  def initialize(digits)
+  def initialize
+    @answers = []
   end
 
-  def find_combinations_for_the_answer(desired_answer)
-    ["1"]
+  def find_combinations_for_the_answer(desired_answer, remaining_string, string_so_far = "")
+    if remaining_string.empty?
+      puts string_so_far
+      puts eval(string_so_far)
+      if eval(string_so_far) == desired_answer
+        @answers << string_so_far
+      end
+      return @answers
+    end
+    # 1 is desired answer. remaining_string is "1". string so far is ""
+    # I want to recur with 0, "", "1"
+    first_number = remaining_string.chars.first.to_i
+    next_string = remaining_string.chars[1..-1].join
+    find_combinations_for_the_answer(desired_answer, next_string, string_so_far << first_number.to_s)
   end
 end
