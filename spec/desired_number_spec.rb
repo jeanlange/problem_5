8436afb9a9e9cba8f2f8b4942bcd9665f6c925a9
Jean Lange <langeje@us.ibm.com>
2018-04-20 23:42:41 -0400

    one more test for a different expected answer

--- spec/desired_number_spec.rb
+++ spec/desired_number_spec.rb
@@ -1,15 +1,22 @@
 require 'desired_number'
 
 describe DesiredNumber do
-  context "using 1 digit: 1" do
-    it "can find one answer expecting 1" do
+  context "using 1 digit" do
+    it "can find one combo that adds up to 1 with the string 1" do
       generator = DesiredNumber.new(1)
       answers = generator.find_combinations_from("1")
       expect(answers.length).to eq 1
       expect(answers).to include("1")
     end
 
-    it "finds no answers expecting 2" do
+    it "can find one combo that adds up to 2 with the string 2" do
+      generator = DesiredNumber.new(2)
+      answers = generator.find_combinations_from("2")
+      expect(answers.length).to eq 1
+      expect(answers).to include("2")
+    end
+
+    it "finds no combos that add up to 2 with the string 1" do
       generator = DesiredNumber.new(2)
       answers = generator.find_combinations_from("1")
       expect(answers.length).to eq 0
